9e0b52d3603ad9e87619f95319133380da81639d
Ace <rossie@Roses-MacBook-Pro.local>
2018-02-27 16:02:00 +0300

    Implement Core data

--- Core-Data/Core-Data/ViewController.swift
+++ Core-Data/Core-Data/ViewController.swift
@@ -1,25 +1,95 @@
-//
-//  ViewController.swift
-//  Core-Data
-//
-//  Created by Ace on 25/02/2018.
-//  Copyright © 2018 Ace. All rights reserved.
-//
-
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var foodItems = [Food]()
+    var moc:NSManagedObjectContext!
+    var appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        moc = appDelegate?.persistentContainer.viewContext
+        self.tableView.dataSource = self
+        
+        loadData()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadData()
+    }
+    
+    func loadData(){
+        let foodRequest:NSFetchRequest<Food> = Food.fetchRequest()
+        
+        let sortDescriptor = NSSortDescriptor(key: "added", ascending: false)
+        foodRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            try foodItems = moc.fetch(foodRequest)
+            
+        }catch {
+            print("Could not load data")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    @IBAction func addFoodToDatabase(_ sender: UIButton) {
+        let foodItem = Food(context: moc)
+        foodItem.added = NSDate() as Date
+        
+        if sender.tag == 0 {
+            foodItem.foodType = "Fruit"
+        }else {
+            foodItem.foodType = "Vegetable"
+        }
+        
+        appDelegate?.saveContext()
+        
+        loadData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // 1
+        let foodItem = foodItems[indexPath.row]
+        
+        // 2
+        let foodType = foodItem.foodType
+        cell.textLabel?.text = foodType
+        
+        // 3
+        let foodDate = foodItem.added as! Date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d yyyy, hh:mm"
+        
+        cell.detailTextLabel?.text = dateFormatter.string(from: foodDate)
+        
+        // 4
+        if foodType == "Fruit" {
+            cell.imageView?.image = UIImage(named: "Apple")
+        }else{
+            cell.imageView?.image = UIImage(named: "Salad")
+        }
+        
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
